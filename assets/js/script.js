4a1091eafeed1b5e53dd04c054453838a69cef63
MackH <96394025+Nesnah1307@users.noreply.github.com>
2022-02-12 19:01:12 -0700

    changed up the html and verified start quiz button is working

---
+++ assets/js/script.js
@@ -0,0 +1,14 @@
+// create variables and questions array
+const startButton = document.getElementById("start-btn")
+startButton.addEventListener("click", startGame)
+// start timer when 'start quiz' is "clicked"
+function startGame(){
+    console.log("started")
+};
+// question is visible and presents another question when answered
+
+// answered incorrectly subtracts timer from
+
+// if timer reaches 0 OR all questions have been answered, game is over
+
+// when the game is over, save initials and score to local storage
