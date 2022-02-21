e1924c0e1edead09d27fc3bfa5fe1bd2c3fa6877
MackH <96394025+Nesnah1307@users.noreply.github.com>
2022-02-20 21:58:43 -0700

    adjusted JS and fixed highscore.html

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,4 +1,4 @@
-// create variables and questions array
+// variables and questions array
 var currentQuestionIndex = 0;
 var questions = [
   {
@@ -78,17 +78,17 @@ function getQuestion() {
   // loop over choices
   currentQuestion.choices.forEach(function (choice, i) {
     // create new button for each choice
-    var choiceNode = document.createElement("button");
-    choiceNode.setAttribute("class", "choice");
-    choiceNode.setAttribute("value", choice);
+    var multiples = document.createElement("button");
+    multiples.setAttribute("class", "choice");
+    multiples.setAttribute("value", choice);
 
-    choiceNode.textContent = i + 1 + ". " + choice;
+    multiples.textContent = i + 1 + ". " + choice;
 
     // attach click event listener to each choice
-    choiceNode.onclick = questionClick;
+    multiples.onclick = questionClick;
 
     // display on the page
-    choicesEl.appendChild(choiceNode);
+    choicesEl.appendChild(multiples);
   });
 }
 
@@ -113,7 +113,7 @@ function questionClick() {
     feedbackEl.textContent = "Correct!";
   }
 
-  // show right or wrong quickly
+  // show right or wrong
   feedbackEl.setAttribute("class", "feedback");
   setTimeout(function () {
     feedbackEl.setAttribute("class", "feedback hide");
@@ -135,6 +135,9 @@ function questionClick() {
 function quizEnd() {
   // stop timer
   clearInterval(timerId);
+  
+  // hide questions section
+  quizScreen.setAttribute("class", "hide");
 
   // show end screen
   var highscoreSectionEl = document.querySelector("#highscore-section");
@@ -144,22 +147,20 @@ function quizEnd() {
   var finalScoreEl = document.querySelector("#final-score");
   finalScoreEl.textContent = time;
 
-  // hide questions section
-  quizScreen.setAttribute("class", "hide");
 }
 
-// function for saving highscore
+// save highscore
 function saveHighscore() {
   // get value of input box
   var initials = initialsEl.value.trim();
 
-  // make sure value wasn't empty
+  // verify value wasn't empty
   if (initials !== "") {
-    // get saved scores from localstorage, or if not any, set to empty array
+    // get saved scores from localstorage
     var highscores =
       JSON.parse(window.localStorage.getItem("highscores")) || [];
 
-    // format new score object for current user
+    
     var newScore = {
       score: time,
       initials: initials
@@ -169,14 +170,12 @@ function saveHighscore() {
     highscores.push(newScore);
     window.localStorage.setItem("highscores", JSON.stringify(highscores));
 
-    // redirect to next page
+    // highscore page
     window.location.href = "highscore.html";
   }
-  console.log(localStorage.getItem('highscores')[0].initials)
-
 }
 
-function checkForEnter(event) {
+function save(event) {
   if (event.key === "Enter") {
     saveHighscore();
   }
